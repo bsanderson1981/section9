8302263a8e3e4bb0c29e24f0b80620fbb227f49b
Bill Sanderson <bill@towniebagels.com>
2025-05-10 13:29:34 -0700

    upated pubspec.yaml to see assests folder with wav files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 //git init && git add . && git commit -m "Initial commit" && git remote add origin https://github.com/bsanderson1981/section8 && git branch -M main && git push -u origin main
-/
+
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
